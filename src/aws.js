faeb8540a632e238e298c0e40edec145f6c44747
Esteve Fernandez <esteve@apache.org>
2025-03-26 16:39:50 +0100

    Added volume size configuration
    Signed-off-by: Esteve Fernandez <esteve@apache.org>

--- src/aws.js
+++ src/aws.js
@@ -73,6 +73,16 @@ async function startEc2Instance(label, githubRegistrationToken) {
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
     InstanceMarketOptions: buildMarketOptions(),
+    BlockDeviceMappings: [
+      {
+        DeviceName: '/dev/sda1',
+        Ebs: {
+          VolumeSize: 50,
+          DeleteOnTermination: true,
+          VolumeType: 'gp3'
+        }
+      }
+    ],
   };
 
   try {
